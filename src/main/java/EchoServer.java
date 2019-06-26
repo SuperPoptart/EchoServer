dec98bcf913e94a67d2187cebd68288a4163f6a7
Richard Sanchez <superpoptart1@gmail.com>
2019-06-26 09:15:49 -0600

    Server should echo endlessly now

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -1,5 +1,28 @@
-public class EchoServer {
-	public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-	}
+public class EchoServer {
+    public static void main(String[] args) throws Exception {
+        short port = -1;
+
+        try {
+            port = Short.parseShort(args[0]);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("invalid port number");
+        }
+
+        if (port < 1025) {
+            throw new Exception("Port must be greater than 1024 and less than " + Short.MAX_VALUE);
+        }
+        ServerSocket serverSocket = new ServerSocket(port);
+        while (true) {//should make the server keep running
+            Socket socket = serverSocket.accept();
+
+            byte[] statement = new byte[200];
+            socket.getInputStream().read(statement);
+
+            socket.getOutputStream().write(statement);
+        }
+    }
 }
